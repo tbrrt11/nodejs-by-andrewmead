5c5ccfaa277622950a0e851a700490cb4ef1020a
wrigo <32571922+tbrrt11@users.noreply.github.com>
2019-05-18 14:26:08 -0300

    class 32

--- weather-app/section6/app.js
+++ weather-app/section6/app.js
@@ -1,11 +1,15 @@
-console.log('Starting')
+const request = require('request')
 
-setTimeout(() => {
-    console.log('2 Seconds Timer')
-}, 2000)
+const url = 'https://api.darksky.net/forecast/4c23c9c89b7284638b635fd89a358211/-25.433984,-49.243957?units=si&lang=pt'
 
-setTimeout(() => {
-    console.log('0 Second Timer')
-}, 0)
-
-console.log('Stopping')
+request({ url: url, json: true}, (error, response) => {
+    const daily = response.body.daily
+    const dayOne = daily.data[0]
+    
+    const currently = response.body.currently
+    const temperature = currently.temperature
+    const precipProbability = currently.precipProbability*100
+    
+    console.log(dayOne.summary + " It is currently " + temperature +
+                " degrees out. There is a " + precipProbability.toFixed(2) + "% chance of rain." )
+})
