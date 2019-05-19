d2a0e685d3ba2b1fcd86ed66cbba1c7ae8e05b4a
wrigo <32571922+tbrrt11@users.noreply.github.com>
2019-05-19 17:09:36 -0300

    isolate geocode

--- weather-app/section6/app.js
+++ weather-app/section6/app.js
@@ -1,35 +1,46 @@
 const request = require('request')
+const geocode = require('./utils/geocode')
 
-const url = 'https://api.darksky.net/forecast/4c23c9c89b7284638b635fd89a358211/-24,25?units=si&lang=pt'
+// const url = 'https://api.darksky.net/forecast/4c23c9c89b7284638b635fd89a358211/-24,25?units=si&lang=pt'
 
-request({ url: url, json: true}, (error, response) => {
+// request({ url: url, json: true}, (error, response) => {
+//     if (error) {
+//         console.log('Unable to connect to weather service.')
+//     } else if (response.body.error || response.body === 'Not Found\n') {
+//         console.log('Unable to find location,', response.body)
+//     } else {
+//         console.log(response.body.daily.data[0].summary + " It is currently " + response.body.currently.temperature + " degrees out. There is a " + (response.body.currently.precipProbability*100).toFixed(2) + "% chance of rain." )
+//     }
+// })
+
+// const address = "rua prefeito angelo lopes 140 curitiba brasil"
+// const pesquisa = address.replace(' ', '%20')
+// const geocodeUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${pesquisa}.json?access_token=pk.eyJ1IjoidGJycnQxMSIsImEiOiJjanZ1djdtaHozdmozNDhxb3BtNWtqcjRpIn0.vbdJu_79Sa9yxcBFdrBbsw&limit=1`
+
+// request({ url: geocodeUrl, json: true}, (error, response) => {
+//     if (error) {
+//         console.log('Unable to connect to geocode service.')
+//     } else if (response.body.message) {
+//         console.log('Unable to find location,', response.body)
+//     } else if (response.body.features.length === 0) {
+//         console.log('Not a single location found for the given address.')
+//     } else {
+//         const coordinates = response.body.features[0].center
+//         const location = response.body.features[0].place_name
+//         const weatherUrl = `https://api.darksky.net/forecast/4c23c9c89b7284638b635fd89a358211/${coordinates[1]},${coordinates[0]}?units=si`
+//         console.log(location)
+//     }
+// })
+
+geocode('jenna marbles', (error, data) => {
     if (error) {
-        console.log('Unable to connect to weather service.')
-    } else if (response.body.error || response.body === 'Not Found\n') {
-        console.log('Unable to find location,', response.body)
+        console.log(error)
     } else {
-        console.log(response.body.daily.data[0].summary + " It is currently " + response.body.currently.temperature + " degrees out. There is a " + (response.body.currently.precipProbability*100).toFixed(2) + "% chance of rain." )
+        console.log(data)
     }
 })
 
-const address = "rua prefeito angelo lopes 140 curitiba brasil"
-const pesquisa = address.replace(' ', '%20')
-const geocodeUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${pesquisa}.json?access_token=pk.eyJ1IjoidGJycnQxMSIsImEiOiJjanZ1djdtaHozdmozNDhxb3BtNWtqcjRpIn0.vbdJu_79Sa9yxcBFdrBbsw&limit=1`
-
-request({ url: geocodeUrl, json: true}, (error, response) => {
-    if (error) {
-        console.log('Unable to connect to geocode service.')
-    } else if (response.body.message) {
-        console.log('Unable to find location,', response.body)
-    } else if (response.body.features.length === 0) {
-        console.log('Not a single location found for the given address.')
-    } else {
-        const coordinates = response.body.features[0].center
-        const location = response.body.features[0].place_name
-        const weatherUrl = `https://api.darksky.net/forecast/4c23c9c89b7284638b635fd89a358211/${coordinates[1]},${coordinates[0]}?units=si`
-        console.log(location)
-    }
-})
+/* THIS IS A TINY EXAMPLE OF WHAT IS CALLED -- THE CALLBACK HELL -- */
 
 // const pesquisa = "paris"
 // const geocodeUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${pesquisa}.json?access_token=pk.eyJ1IjoidGJycnQxMSIsImEiOiJjanZ1djdtaHozdmozNDhxb3BtNWtqcjRpIn0.vbdJu_79Sa9yxcBFdrBbsw&limit=1`
