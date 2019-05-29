c7da0ada1264f436bfb5a97aa3171af6959a392c
wrigo <32571922+tbrrt11@users.noreply.github.com>
2019-05-28 21:31:00 -0300

    add cmd attr parsing

--- weather-app/section6/app.js
+++ weather-app/section6/app.js
@@ -1,34 +1,51 @@
-const request = require('request')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
+const yargs = require('yargs')
 
-// geocode('jenna marbles', (error, data) => {
-//     if (error) {
-//         console.log(error)
-//     } else {
-//         console.log(data)
-//     }
-// })
-
-// forecast(-75.7088, 44.1545, (error, data) => {
-//     if (error) {
-//         console.log(error)
-//     } else {
-//         console.log(data)
-//     }
-// })
-
-geocode('curitiba rua prefito angelo lopes 140', (error, data) => {
-    if (error) {
-        console.log(error)
-    } else {
-        console.log("Forecast for", data.location)
-        forecast(data.latitude, data.longitude, (error, data) => {
+yargs.command({
+    command: 'forecast',
+    describe: 'Get forecast for an address',
+    builder: {
+        address: {
+            describe: 'Address to be searched',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv){
+        geocode(argv.address, (error, data) => {
             if (error) {
                 console.log(error)
             } else {
-                console.log(data)
+                console.log("Forecast for", data.location)
+                forecast(data.latitude, data.longitude, (error, data) => {
+                    if (error) {
+                        console.log(error)
+                    } else {
+                        console.log(data)
+                    }
+                })
             }
         })
     }
 })
+
+// yargs.parse()
+
+const address = process.argv[2]
+
+if (!address) {
+    console.log('Please provide an address.')
+} else {
+    geocode(process.argv[2], (error, geocodeData) => {
+        if (error) {
+           return console.log(error)
+        }
+        forecast(geocodeData.latitude, geocodeData.longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
+            console.log("Forecast for", geocodeData.location, '\n', forecastData)
+        })
+    })    
+}
