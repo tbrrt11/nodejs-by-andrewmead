97743ade8f1d0d9abdb41cb9752047debbba6906
tbrrt11 <32571922+tbrrt11@users.noreply.github.com>
2019-06-04 00:44:39 -0300

    aula 48

--- web-server/section7/src/app.js
+++ web-server/section7/src/app.js
@@ -1,23 +1,37 @@
 const path = require('path')
 const express = require('express')
 
-console.log(__dirname)
-console.log(path.join(__dirname, '../public'))
-
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Thiago Barreto'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Thiago Barreto'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help Page'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
         forecast: 'This is your forecast.',
         location: 'Curitiba, Brazil'
     })
-})
-
-app.get('/weather', (req, res) => {
-    res.send('Watch out for the rain that is coming.')
 })
 
 // app.com
