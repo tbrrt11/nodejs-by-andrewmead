0b652faef62e6bbcf9ec183891394e091a57b50a
tbrrt11 <32571922+tbrrt11@users.noreply.github.com>
2019-08-07 03:41:32 -0300

    aula 49

--- web-server/section7/src/app.js
+++ web-server/section7/src/app.js
@@ -1,10 +1,20 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
-const publicDirectoryPath = path.join(__dirname, '../public')
 
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../mvc/views')
+const partialsPath = path.join(__dirname, '../mvc/partials')
+
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
@@ -23,7 +33,8 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        title: 'Help Page'
+        title: 'Help Page',
+        name: 'Thiago Barreto'
     })
 })
 
