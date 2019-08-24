291071d770952435f48c217e2a5a00af794e76f9
tbrrt11 <32571922+tbrrt11@users.noreply.github.com>
2019-08-24 09:42:19 -0300

    aula 80

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -23,27 +23,27 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     const db = client.db(databaseName)
 
-    // db.collection('users').findOne({ name: 'Andrew'}, (error, user) => {
-    //     if (error) {
-    //         return console.log('Unable to fetch', error)
+    // db.collection('users').updateOne({
+    //     _id: ObjectID("5d55214a37b7184a2cdeb88b")
+    // }, {
+    //     $inc: {
+    //         age: 1
     //     }
-
-    //     console.log(user)
+    // }).then((result) => {
+    //     console.log(result)
+    // }).catch((error) => {
+    //     console.log(error)
     // })
 
-    // db.collection('users').find({ age: 27}).toArray((error, users) => {
-    //     console.log(users)
-    // })
-
-    // db.collection('users').find({ age: 27}).count((error, count) => {
-    //     console.log(count)
-    // })
-
-    db.collection('tasks').findOne({ _id: new ObjectID("5d566b3198c9ae59ea626ec1") }, (error, task) => {
-        console.log(task)
-    })
-
-    db.collection('tasks').find({ completed: false}).toArray((error, tasks) => {
-        console.log(tasks)
+    db.collection('tasks').updateMany({
+        completed: false
+    }, {
+        $set: {
+            completed: true
+        }
+    }).then((result) => {
+        console.log(result)
+    }).catch((error) => {
+        console.log(error)
     })
 })
