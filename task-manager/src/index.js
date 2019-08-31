087e5450dad54cd6be0fc264d130947fa8f6a73b
tbrrt11 <32571922+tbrrt11@users.noreply.github.com>
2019-08-31 12:49:03 -0300

    aula 92

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 require('./db/mongoose')
 const User = require('./models/user')
+const Task = require('./models/task')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -11,9 +12,64 @@ app.post('/users', (req, res) => {
     const user = new User(req.body)
 
     user.save().then(() => {
+        res.status(201).send(user)
+    }).catch((error) => {
+        res.status(400).send({ error: error.message })
+    })
+})
+
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch((error) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+
+    User.findById(_id).then((user) => {
+        if (!user){
+            return res.status(404).send()
+        }
+
         res.send(user)
     }).catch((error) => {
-        res.status(400).send({error: error.message})
+        res.status(500).send()
+    })
+})
+
+app.post('/tasks', (req, res) => {
+    const task = new Task(req.body)
+
+    task.save().then(() => {
+        res.status(201).send(task)
+    }).catch((error) => {
+        res.status(400).send({ error: error.message })
+    })
+})
+
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((error) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id
+
+    Task.findById(_id).then((task) => {
+        if (!_id){
+            return res.status(404).send()
+        }
+        
+        res.send(task)
+    }).catch((error) => {
+        res.status(500).send()
     })
 })
 
