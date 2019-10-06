111baba32af6fb691f3493b348c51b05b87f8dc6
tbrrt11 <32571922+tbrrt11@users.noreply.github.com>
2019-10-06 11:44:16 -0300

    aula 114

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -6,18 +6,6 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-// app.use((req, res, next) => {
-//     if (req.method == 'GET') {
-//         res.send('GET requests are disabled')
-//     } else {
-//         next()
-//     }
-// })
-
-// app.use((req, res, next) => {
-//     res.status(503).send('Please wait. The servers are being enhanced')
-// })
-
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -25,3 +13,18 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
+
+const Task = require('./models/task')
+const User = require('./models/user')
+
+const main = async () => {
+    // const task = await Task.findById('5d99f88e1995b0644d064335')
+    // await task.populate('owner').execPopulate()
+    // console.log(task.owner)
+
+    const user = await User.findById('5d99f68fad508a6333e60c83')
+    await user.populate('tasks').execPopulate()
+    console.log(user.tasks)
+}
+
+main()
