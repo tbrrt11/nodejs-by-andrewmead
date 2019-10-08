ef408984e69fe8c2d3d07f2bbe4b70529aa44fc8
tbrrt11 <32571922+tbrrt11@users.noreply.github.com>
2019-10-08 03:21:40 -0300

    aula 121

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -18,10 +18,32 @@ router.post('/tasks', auth, async (req, res) => {
     }
 })
 
-
+// GET /tasks?completed=true
+// GET /tasks?limit=10&skip=10
+// GET /tasks?sortBy=createdAt:desc
 router.get('/tasks', auth, async (req, res) => {
+    const match = {}
+    const sort = {}
+
+    if (req.query.completed) {
+        match.completed = req.query.completed === 'true'
+    }
+
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split(':')
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+    }
+
     try {
-        await req.user.populate('tasks').execPopulate() //alternative: const task = await Task.find({ owner: req.user._id })
+        await req.user.populate({
+            path: 'tasks',
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip),
+                sort
+            }
+        }).execPopulate() //alternative: const task = await Task.find({ owner: req.user._id })
         res.send(req.user.tasks)
     } catch (e) {
         res.status(500).send()
