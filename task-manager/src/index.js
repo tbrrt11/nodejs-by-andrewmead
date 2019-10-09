3006b696ec626f75ed16642a20f9ad49f7573b6a
tbrrt11 <32571922+tbrrt11@users.noreply.github.com>
2019-10-09 20:53:11 -0300

    aula 128

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -20,8 +20,13 @@ const upload = multer({
         cb(undefined, true)
     }
 })
+const errorMiddleware = (req, res, next) => {
+    throw new Error('From my middleware')
+}
 app.post('/upload', upload.single('upload'), (req, res) => {
     res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
 })
 
 
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -79,11 +79,45 @@ router.delete('/users/me', auth, async (req, res) => {
 })
 
 uploadAvatar = multer({
-    dest: 'avatars'
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter (req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            cb(new Error('Please upload an image.'))
+        }
+
+        cb(undefined, true)
+    }
 })
 
-router.post('/users/me/avatar', auth, uploadAvatar.single('avatar'), (req, res) => {
+router.post('/users/me/avatar', auth, uploadAvatar.single('avatar'), async (req, res) => {
+    req.user.avatar = req.file.buffer
+    await req.user.save()
+    res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
+})
+
+router.delete('/users/me/avatar', auth, async (req, res) => {
+    req.user.avatar = undefined
+    await req.user.save()
     res.send()
 })
 
-module.exports = router
+router.get('/users/:id/avatar', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+
+        if (!user || !user.avatar) {
+            throw new Error()
+        }
+
+        res.set('Content-Type', 'image/jpg')
+        res.send(user.avatar)
+    } catch (e) {
+        res.status(404).send()
+    }
+})
+
+module.exports = router 
