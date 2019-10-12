8ad742522a1d514a29b58ce1e0c6b38fde494c84
tbrrt11 <32571922+tbrrt11@users.noreply.github.com>
2019-10-12 19:19:21 -0300

    aula 133

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -3,11 +3,13 @@ const multer = require('multer')
 const sharp = require('sharp')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
+const { sendWelcomeEmail, sendGoodbyeEmail } = require('../emails/account')
 const router = new express.Router()
 
 router.post('/users', async (req, res) => {
     try {
         const user = new User(req.body)
+        sendWelcomeEmail(user.email, user.name)
         await user.save()
         const token = await user.generateAuthToken()
         res.status(201).send({ user, token })
@@ -73,6 +75,7 @@ router.patch('/users/me', auth, async (req,res) => {
 router.delete('/users/me', auth, async (req, res) => {
     try {
         await req.user.remove()
+        sendGoodbyeEmail(req.user.email, req.user.name)
         res.send(req.user) 
     } catch (e) {
         res.status(500).send()
