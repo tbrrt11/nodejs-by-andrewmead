d1879b707e1cfc8dd28547b16e2b2febc465ce38
tbrrt11 <32571922+tbrrt11@users.noreply.github.com>
2019-10-20 19:01:31 -0300

    aula 147

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -93,3 +93,36 @@ test('Should not delete account for unauthenticated user', async () => {
         .send()
         .expect(401)
 })
+
+test('Should upload avatar image', async () => {
+    await request(app)
+        .post('/users/me/avatar')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .attach('avatar', 'tests/fixtures/profile-pic.jpg')
+        .expect(200)
+    const user = await User.findById(userOneId)
+    expect(user.avatar).toEqual(expect.any(Buffer))
+})
+
+test('Should update valid user fields', async () => {
+    await request(app)
+        .patch('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            name: 'Thiago Trystan'
+        })
+        .expect(200)
+
+    const user = await User.findById(userOneId)
+    expect(user.name).toBe('Thiago Trystan')
+})
+
+test('Should not update invalid user fields', async () => {
+    await request(app)
+        .patch('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            localization: 'Manaus'
+        })
+        .expect(400)
+})
