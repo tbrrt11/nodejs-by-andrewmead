06d93066cbc725781bfaadd714f30da4a4a66b66
tbrrt11 <32571922+tbrrt11@users.noreply.github.com>
2019-10-27 11:04:40 -0300

    aula 172

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -35,6 +35,10 @@ io.on('connection', (socket) => {
 
         socket.emit('message', generateMessage('Admin', "Welcome!"))
         socket.broadcast.to(user.room).emit('message', generateMessage('Admin' ,`${user.username} has joined!`))
+        io.to(user.room).emit('roomData', {
+            room: user.room,
+            users: getUsersInRoom(user.room)
+        })
 
         callback()
     })
@@ -66,6 +70,10 @@ io.on('connection', (socket) => {
 
         if (user) {
             io.to(user[0].room).emit('message', generateMessage('Admin', `${user[0].username} has left!`))
+            io.to(user[0].room).emit('roomData', {
+                room: user[0].room,
+                users: getUsersInRoom(user[0].room)
+            })
         }
     }) 
 })
